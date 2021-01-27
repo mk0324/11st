7ab4797a53af01c8c0b4fd8d97165a43994c31b5
munkyung <munkyung0324@gmail.com>
2021-01-27 10:13:47 +0900

    feat: create Billboard

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <l-n-b></l-n-b>
     <my-header></my-header>
+    <billboard></billboard>
   </div>
 </template>
 
 <script>
 import LNB from "./components/LNB";
 import MyHeader from "./components/MyHeader";
+import Billboard from "./components/Billboard";
 export default {
   components: {
     LNB,
-    MyHeader
+    MyHeader,
+    Billboard,
   }
 }
 </script>
